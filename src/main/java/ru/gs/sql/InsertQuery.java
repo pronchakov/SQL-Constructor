8807886c7bb4a76b3e35c7b169fc49d9be59ebfd
Artem Pronchakov <artem.pronchakov@gmail.com>
2011-10-15 20:10:25 +0400

    Some Insert class implementation added

--- src/main/java/ru/gs/sql/InsertQuery.java
+++ src/main/java/ru/gs/sql/InsertQuery.java
@@ -2,13 +2,15 @@ package ru.gs.sql;
 
 import java.util.Arrays;
 import java.util.List;
+import ru.gs.sql.exceptions.SQLCreationException;
 
 /**
+ * Class for building SQL INSERT queries.
  *
  * @author APronchakov <artem.pronchakov@gmail.com>
  */
 public class InsertQuery extends CommonQuery {
-    
+
     public InsertQuery() {
         queryBuilder.append("INSERT INTO ");
     }
@@ -19,28 +21,52 @@ public class InsertQuery extends CommonQuery {
         queryBuilder.append(" ");
     }
 
-    public InsertQuery addTableName(String name) {
+    public InsertQuery addTableName(String name) throws SQLCreationException {
+        if (isStringEmptyOrNull(name)) {
+            throw new SQLCreationException("Field name cannot be null or empty");
+        }
         queryBuilder.append(name);
         queryBuilder.append(" ");
         return this;
     }
+    
+    private boolean isStringEmptyOrNull(String string) {
+        if (string == null || string.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-    public void addInsertableFieldNames(String[] names) {
+    public void addInsertableFieldNames(String[] names) throws SQLCreationException {
         addInsertableFieldNamesCommonMethod(Arrays.asList(names));
     }
 
-    public void addInsertableFieldNames(List<String> names) {
+    public void addInsertableFieldNames(List<String> names) throws SQLCreationException {
         addInsertableFieldNamesCommonMethod(names);
     }
 
-    private void addInsertableFieldNamesCommonMethod(List<String> names) {
+    private void addInsertableFieldNamesCommonMethod(List<String> names) throws SQLCreationException {
         queryBuilder.append("(");
-        for(String name: names) {
+        for (String name : names) {
             queryBuilder.append(name);
             queryBuilder.append(",");
         }
         deleteLastCommaIFExist();
         queryBuilder.append(") ");
     }
-    
+
+    public void startAddingInsertableFieldNames() {
+        queryBuilder.append("(");
+    }
+
+    public void stopAddingInsertableFieldNames() {
+        deleteLastCommaIFExist();
+        queryBuilder.append(") ");
+    }
+
+    public void addInsertableFieldName(String name) {
+        queryBuilder.append(name);
+        queryBuilder.append(",");
+    }
 }
